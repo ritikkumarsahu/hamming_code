cb4f75e27fd58d7f5b6611716c9d8017686fea27
ritikkumarsahu <ritikrks@gmail.com>
2020-11-09 22:55:02 +0530

    added navbar active link feature

--- app/routes/views.py
+++ app/routes/views.py
@@ -4,3 +4,7 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/visualise')
+def visualise():
+    return render_template('visualise.html')
