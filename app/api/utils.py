cb5079f455c847ad1bacecdd8a1a0d1118e7bd6a
ritikkumarsahu <ritikrks@gmail.com>
2020-11-25 09:56:10 +0530

    commit-2

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,12 +1,15 @@
 from app import app
 from math import log2
 from flask import request
+import json
 
 @app.route('/get_bin', methods=['POST'])
 def get_bin(num=None):
     if request.method == "POST":
-        num = request.form.get("data")
-        grid_size = request.form.get("grid_size")
+        data = json.loads(request.data)
+        print(data)
+        num = data.get("data")
+        grid_size = data.get("grid_size")
         if grid_size:
             return find_bin(int(num), int(grid_size))
         return find_bin(num)
@@ -26,9 +29,9 @@ def calc_bin():
 def find_bin (num, grid_size = None):
     if grid_size:
         bin_num = bin(num)[2:]
-        return bin_num if len(bin_num) >= grid_size else '0' * (grid_size - len(bin_num)) + bin_num
+        return {'bin_num' : bin_num if len(bin_num) >= grid_size else '0' * (grid_size - len(bin_num)) + bin_num}
     else:
-        return ' '.join(format(i, 'b') for i in bytearray(num, encoding ='utf-8')) 
+        return {'bin_num':' '.join(format(i, 'b') for i in bytearray(num, encoding ='utf-8'))} 
 
 def bin2str(bin):
-    return "".join(map(lambda x: chr(int(x,2)), bin.split()))
+    return {'string':"".join(map(lambda x: chr(int(x,2)), bin.split()))}
