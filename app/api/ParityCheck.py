4c2a419a62dda3421c2c95eaa018a1912f20b43a
ritikkumarsahu <ritikrks@gmail.com>
2020-12-05 11:12:29 +0530

    ver 3

--- app/api/ParityCheck.py
+++ app/api/ParityCheck.py
@@ -3,5 +3,7 @@ from functools import reduce
 def parity_check(bits):
     return reduce(
         lambda x,y: x^y,
-        [i for (i,b) in enumerate(bits) if b]
+        [i for (i,b) in enumerate(bits) if int(b)]
     )
+if __name__ == "__main__":
+    print(parity_check("11101111110"))
